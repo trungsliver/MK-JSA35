7bd72ef7e49ef98f06e64cf8b493eaf94aae4e8e
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-19 11:42:56 +0700

    Lesson 5

--- Lesson5/script.js
+++ Lesson5/script.js
@@ -16,3 +16,30 @@ students.forEach(student => {
     // Thêm thẻ vào container
     container1.appendChild(studentDiv);
 });
+
+// Bài 2: Hiển thị dữ liệu từ file data.json
+    // Lấy dữ liệu từu file data.json
+fetch('./data.json')
+    // Chuyển đổi dữ liệu sang dạng JSON
+    .then(response => response.json())
+    // Xử lý dữ liệu sau khi chuyển đổi
+    .then(data => {
+        // Dùng DOM lấy container
+        const container2 = document.getElementById('productList');
+
+        // Duyệt danh sách sản phẩm từ file data.json
+        data.forEach(item => {
+            // Tạo thẻ div cho từng sản phẩm
+            const itemDiv = document.createElement('div');
+            // Thêm class cho từng thẻ itemDiv
+            itemDiv.classList.add('product-card');
+            // Thêm thông tin sản phẩm vào thẻ
+            itemDiv.innerHTML = `
+                <img src="${item.image}" alt="${item.name}">
+                <h3>${item.name}</h3>
+                <p>Price: ${item.price}</p>
+            `;
+            // Thêm thẻ vào container
+            container2.appendChild(itemDiv);
+        })
+    })
