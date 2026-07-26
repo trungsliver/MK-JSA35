dc6f7b268f0b1d0fd00591aab805b5e97b9f3fd7
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-07-26 10:41:31 +0700

    Lesson 6

---
+++ Lesson6/script.js
@@ -0,0 +1,28 @@
+// bài 1: Lấy dữ liệu từ API 
+    // Gọi API
+fetch('https://jsonplaceholder.typicode.com/todos')
+    // Chuyển đổi dữ liệu thành dạng JSON
+    .then(response => response.json())
+    // Xử lý dữ liệu
+    .then(data => {
+        // Dùng DOM lấy container
+        const container = document.getElementById('todo-list');
+
+        // Duyệt dữ liệu
+        for (let i = 0; i < 20; i++) {
+            // Tạo thẻ div có class = 'card' cho mỗi phần tử
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            // Thêm nội dung vào card
+            card.innerHTML = `
+                <h3>${data[i].title}</h3>
+                <p>ID: ${data[i].id}</p>
+                <p>User ID: ${data[i].userId}</p>
+                <p>Completed: ${data[i].completed}</p>
+            `;
+
+            // Thêm card vào container
+            container.appendChild(card);
+        }
+    })
